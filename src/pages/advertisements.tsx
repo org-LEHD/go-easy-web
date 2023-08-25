aec33c73f55edc92e330c57fbb677517344c2e53
EGalvit <54305201+EGalvit@users.noreply.github.com>
2023-08-25 11:43:47 +0200

    Advertisements table added

--- src/pages/advertisements.tsx
+++ src/pages/advertisements.tsx
@@ -1,30 +1,27 @@
 import { useSession } from "next-auth/react";
 import Login from "./login";
 import { Box, Button, Container, Flex, Grid, Table } from "@mantine/core";
+import { api } from "~/utils/api";
+import { AdvertisementTable } from "~/common/components/AdvertisementTable";
 
 const Advertisements: React.FC = () => {
   const { data: sessionData } = useSession();
   if (sessionData === null) {
     return <Login />;
   }
+  const { data: advertisements, isLoading } = api.advertisement.getAllById.useQuery(
+    sessionData?.user?.id ?? 0,
+    { enabled: sessionData?.user.id !== undefined }
+  );
+
+  if (sessionData?.user === undefined) return <Login />;
+  if (isLoading) return <div>Im loading</div>;
 
   return (
     <Container>
-      <Flex>
+      <Flex direction={"column"} gap={"md"}>
         <h1>Annoncer</h1>
-        <Table>
-          <thead>
-            <tr>
-              <th>AnnonceID</th>
-              <th>Navn</th>
-              <th>Start</th>
-              <th>Slut</th>
-              <th>Forretning</th>
-              <th>Oprettet</th>
-              <th> </th>
-            </tr>
-          </thead>
-        </Table>
+        <AdvertisementTable advertisements={advertisements as any} />
         <Button>Ny Annonce</Button>
       </Flex>
     </Container>
