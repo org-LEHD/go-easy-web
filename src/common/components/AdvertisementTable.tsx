eeac530e07404fea8b07b3e2b8903dc8299a32b8
lha <lha@projectum.com>
2023-09-07 11:18:53 +0200

    Added further fields to Location- and AdvertisementTables.

--- src/common/components/AdvertisementTable.tsx
+++ src/common/components/AdvertisementTable.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Table } from "@mantine/core";
+import { NavLink, Table, Tooltip, Text, Anchor } from "@mantine/core";
 import { IconArrowRight } from "@tabler/icons";
 import { formatDate, formatDateTime } from "~/utils/dateFormatter";
 import { useRouter } from "next/router";
@@ -11,17 +11,17 @@ export const AdvertisementTable: React.FC<AdvertisementTableProps> = ({
   advertisements,
 }) => {
   const router = useRouter();
-  const isAdvertiserPage = router.asPath.startsWith('/advertiser/locations')
-  // const { data: sessionData } = useSession();
-  // const isAdmin = sessionData?.user.role === "Administrator";
+  const isAdvertiserPage = router.asPath.startsWith("/advertiser/locations");
 
   return (
     <Table>
       <thead>
         <tr>
-          <th>AnnonceID</th>
+          <th>ID</th>
           <th>Navn</th>
           <th>Forretning</th>
+          <th>Beskrivelse</th>
+          <th>Medie</th>
           <th>Start</th>
           <th>Slut</th>
           <th>Oprettet</th>
@@ -35,17 +35,31 @@ export const AdvertisementTable: React.FC<AdvertisementTableProps> = ({
             <td>{advertisement.id}</td>
             <td>{advertisement.title}</td>
             <td>{advertisement.location.name}</td>
+            <td style={{ maxWidth: 200 }}>
+              <Tooltip label={advertisement.description}>
+                <Text truncate>{advertisement.description}</Text>
+              </Tooltip>
+            </td>
+            <td style={{ maxWidth: 200 }}>
+              <Anchor href={advertisement.media} target={"_blank"}>
+                <Tooltip label={advertisement.media}>
+                  <Text truncate>{advertisement.media}</Text>
+                </Tooltip>
+              </Anchor>
+            </td>
             <td>{`${formatDateTime(advertisement.start)}`}</td>
             <td>{`${formatDateTime(advertisement.end)}`}</td>
             <td>{`${formatDate(advertisement.createdAt)}`}</td>
             <td>{`${formatDate(advertisement.updatedAt)}`}</td>
-            {!isAdvertiserPage && <td>
-              <NavLink
-                href={`/advertisement/${advertisement.id}`}
-                component="a"
-                icon={<IconArrowRight color="blue" />}
-              />
-            </td>}
+            {!isAdvertiserPage && (
+              <td>
+                <NavLink
+                  href={`/advertisement/${advertisement.id}`}
+                  component="a"
+                  icon={<IconArrowRight color="blue" />}
+                />
+              </td>
+            )}
           </tr>
         ))}
         {advertisements?.length === 0 && (
